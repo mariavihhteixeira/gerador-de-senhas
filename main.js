cd46fb43c7769382a1d08dc71da2ca2a9c625144
mariavihhteixeira <maria.teixeira03@escola.pr.gov.br>
2026-09-10 09:55:12 -0300

    Add password generation functionality

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const numeroSenha= document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent= tamanhoSenha;
 
+const letrasMaiusculas= 'ABCDEFGHIJKLMNOPQQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros= '123456789';
+const simbolos= '!?*@%';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha= document.querySelector('#campo-senha');
+const checkbox= document.querySelectorAll('.checkbox');
+const forcaSenha= document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick =  aumentaTamanho;
